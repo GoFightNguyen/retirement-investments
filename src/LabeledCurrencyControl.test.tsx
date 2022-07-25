950899e3330ac2cebe81c87103b6f139948dccad
GoFightNguyen <jsn.dev@outlook.com>
2022-07-25 16:07:03 -0600

    test(LabeledCurrencyControl): onMoniesChange is triggered

--- src/LabeledCurrencyControl.test.tsx
+++ src/LabeledCurrencyControl.test.tsx
@@ -17,33 +17,57 @@ describe("LabeledCurrencyControl", () => {
   });
 
   describe.each`
-    userInput     | expected
-    ${".1"}       | ${"$.10"}
-    ${"3"}        | ${"$3.00"}
-    ${"3.0"}      | ${"$3.00"}
-    ${"3.57"}     | ${"$3.57"}
-    ${"3.574"}    | ${"$3.57"}
-    ${"3.578"}    | ${"$3.57"}
-    ${"$3.57"}    | ${"$3.57"}
-    ${"6153"}     | ${"$6,153.00"}
-    ${"6,153"}    | ${"$6,153.00"}
-    ${"9876,153"} | ${"$9,876,153.00"}
-  `("should format user input", ({ userInput, expected }) => {
-    test(`
-    Given the user inputs ${userInput}
-    Then ${expected} is displayed`, async () => {
-      const user = userEvent.setup();
-      render(
-        <LabeledCurrencyControl
-          id="testCurrency"
-          labelText="Test Currency"
-          onMoniesChange={() => {}}
-        />
-      );
-      const input = screen.getByLabelText("Test Currency");
-      await user.clear(input);
-      await user.type(input, userInput);
-      expect(input).toHaveValue(expected);
-    });
-  });
+    userInput     | expectedDisplayValue | expectedTriggerValue
+    ${".1"}       | ${"$.10"}            | ${0.1}
+    ${"3"}        | ${"$3.00"}           | ${3}
+    ${"3.0"}      | ${"$3.00"}           | ${3.0}
+    ${"3.57"}     | ${"$3.57"}           | ${3.57}
+    ${"3.574"}    | ${"$3.57"}           | ${3.57}
+    ${"3.578"}    | ${"$3.57"}           | ${3.57}
+    ${"$3.57"}    | ${"$3.57"}           | ${3.57}
+    ${"6153"}     | ${"$6,153.00"}       | ${6153}
+    ${"6,153"}    | ${"$6,153.00"}       | ${6153}
+    ${"9876,153"} | ${"$9,876,153.00"}   | ${9876153}
+  `(
+    "should handle user input",
+    ({ userInput, expectedDisplayValue, expectedTriggerValue }) => {
+      function setup() {
+        const mockTrigger = jest.fn();
+        const user = userEvent.setup();
+        const utils = render(
+          <LabeledCurrencyControl
+            id="testCurrency"
+            labelText="Test Currency"
+            onMoniesChange={mockTrigger}
+          />
+        );
+
+        const input = screen.getByLabelText("Test Currency");
+        const changeCurrencyInput = async (value: string) => {
+          await user.clear(input);
+          await user.type(input, value);
+        };
+
+        return { ...utils, mockTrigger, input, changeCurrencyInput };
+      }
+
+      test(`
+      Given the user inputs ${userInput}
+      Then ${expectedDisplayValue} is displayed`, async () => {
+        const utils = setup();
+        await utils.changeCurrencyInput(userInput);
+        expect(utils.input).toHaveValue(expectedDisplayValue);
+      });
+
+      test(`
+      Given the user inputs ${userInput}
+      Then onMoniesChange is triggered with ${expectedTriggerValue}`, async () => {
+        const utils = setup();
+        await utils.changeCurrencyInput(userInput);
+        expect(utils.mockTrigger).toHaveBeenLastCalledWith(
+          expectedTriggerValue
+        );
+      });
+    }
+  );
 });

216e449121d48e434e4da8a9dac4f7d4ad68a34e
GoFightNguyen <jsn.dev@outlook.com>
2022-07-25 18:00:15 -0600

    refactor(app): use mui
    https://github.com/mui/material-ui/tree/master/examples/create-react-app-with-typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import LabeledCurrencyControl from "./LabeledCurrencyControl";
 import NumberFormat from "react-number-format";
 import { useState } from "react";
+import { Box, Container, Typography } from "@mui/material";
 
 function App() {
   const [annualSalary, setAnnualSalary] = useState(0);
@@ -10,24 +11,29 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <LabeledCurrencyControl
-        id="annualSalary"
-        labelText="Annual Salary"
-        onMoniesChange={(monies) => handleAnnualSalaryChange(monies)}
-      />
-      <span>
-        monies to invest in retirement
-        <NumberFormat
-          displayType={"text"}
-          decimalScale={2}
-          fixedDecimalScale
-          thousandSeparator={true}
-          prefix={"$"}
-          value={annualSalary * 0.15}
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Retirement Investments
+        </Typography>
+        <LabeledCurrencyControl
+          id="annualSalary"
+          labelText="Annual Salary"
+          onMoniesChange={(monies) => handleAnnualSalaryChange(monies)}
         />
-      </span>
-    </div>
+        <span>
+          monies to invest in retirement
+          <NumberFormat
+            displayType={"text"}
+            decimalScale={2}
+            fixedDecimalScale
+            thousandSeparator={true}
+            prefix={"$"}
+            value={annualSalary * 0.15}
+          />
+        </span>
+      </Box>
+    </Container>
   );
 }
 

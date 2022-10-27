f48fcb322cd3f2ba520ac7c369993870891abf26
GoFightNguyen <jsn.dev@outlook.com>
2022-10-27 16:54:06 -0600

    refactor(add-investment): extract handleSubmit and remove export of AddInvestmentProps

--- src/components/AddInvestment.tsx
+++ src/components/AddInvestment.tsx
@@ -1,10 +1,10 @@
 import { Button, TextField } from "@mui/material";
-import { useState } from "react";
+import React, { useState } from "react";
 
 /**
  * Props for {@link AddInvestment}
  */
-export interface AddInvestmentProps {
+interface AddInvestmentProps {
   /**
    * The function to call when the investment is added
    */
@@ -19,13 +19,13 @@ export interface AddInvestmentProps {
 const AddInvestment = (props: AddInvestmentProps) => {
   const [name, setName] = useState("");
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    props.onInvestmentAdded(name);
+  };
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        props.onInvestmentAdded(name);
-      }}
-    >
+    <form onSubmit={(e) => handleSubmit(e)}>
       <TextField
         required
         label="Investment"
